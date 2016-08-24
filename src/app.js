4343cb39a100c1ec6fe3aa7618dedae262280f33
jackhkmatthews <jackhkmatthews@gmail.com>
2016-08-24 22:59:40 +0100

    blog route, page under construction, error status

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
 'use strict';
 
 var express = require('express');
+var posts = require('./mock/posts.json');
 
 var app = express();
 
 app.get('/', function(req, res){
-  res.send("<h1>I Love Treehouse!</h1>");
+  res.send("<h1>I'm in love with Treehouse!</h1>");
+});
+
+app.get('/blog/:title?', function(req, res){
+  var title = req.params.title;
+  if (title === undefined){
+    res.status(503);
+    res.send("this page is under construction");
+  } else {
+  var post = posts[title];
+  res.send(post);
+  }
 });
 
 app.listen(3000, function(){
